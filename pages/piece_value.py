791cb4cc9e18bc3f38f0cf2592acf2dde1a59a82
fsmosca <fsmosca@users.noreply.github.com>
2024-06-10 23:19:02 +0800

    Update piece_value.py

--- pages/piece_value.py
+++ pages/piece_value.py
@@ -5,6 +5,7 @@ from module.nav import Nav
 import pandas as pd
 import urllib.request
 import urllib.error
+from module.data import PT_IMAGE
 
 
 st.set_page_config(layout='wide', page_title=st.secrets['page_title'])
@@ -19,23 +20,6 @@ st.markdown("""
             }
     </style>
     """, unsafe_allow_html=True)
-
-
-pt_image = {
-    'Cannon': 'https://camo.githubusercontent.com/cd95c3c50722f0d876a335665daab55c2efa82195fa3eb673ba5fab76c0a29df/68747470733a2f2f692e696d6775722e636f6d2f4d3176394c5a6a2e706e67',
-    'Leopard': 'https://camo.githubusercontent.com/808307273e0738f065680ba457bffa98ff7ac333df40035f9127b62902c5a59b/68747470733a2f2f692e696d6775722e636f6d2f346d36414f7a632e706e67',
-    'Unicorn': 'https://camo.githubusercontent.com/cc4a9207d935eb4692228d92ecf46a52672be7269c51df32d9f5329ffd87cdfe/68747470733a2f2f692e696d6775722e636f6d2f617853635632752e706e67',
-    'Dragon': 'https://camo.githubusercontent.com/b6dad9d3400b6b725055176328d6e4650e0dcebc4bc352038b5ec435ae2f61bb/68747470733a2f2f692e696d6775722e636f6d2f4b4262307536552e706e67',
-    'Chancellor': 'https://camo.githubusercontent.com/bcaafe790474dd99b7a857d2b4efb832529573d5d045bde980954420f6d77e8d/68747470733a2f2f692e696d6775722e636f6d2f346a5a4c7a48422e706e67',
-    'Archbishop': 'https://camo.githubusercontent.com/fe5c5ce719f871c981f5c5325ffdf85cc8f078dfc8a245cc06eceb9bedaf7f61/68747470733a2f2f692e696d6775722e636f6d2f6f476531676b632e706e67',
-    'Elephant': 'https://camo.githubusercontent.com/4ea2eef6e9c9d0b8a982eb97e2ed263a4a93bcfe300a6df28e4591a9767ea021/68747470733a2f2f692e696d6775722e636f6d2f6a327767464c652e706e67',
-    'Hawk': 'https://camo.githubusercontent.com/de904be16c8c43007d1e8bd32d3e4ca3567a623232e3553971f0273092009e22/68747470733a2f2f692e696d6775722e636f6d2f45576d6c616c772e706e67',
-    'Fortress': 'https://camo.githubusercontent.com/91070e29832e6f236dfa72241a3186cf10f5201aa22d20243031f59fe7b61302/68747470733a2f2f692e696d6775722e636f6d2f5930726e686b342e706e67',
-    'Spider': 'https://camo.githubusercontent.com/98fc95a54ebb8a11aab6a367af39868a1268f7089eb76e10b3e79928397090ab/68747470733a2f2f692e696d6775722e636f6d2f75434438704f412e706e67',
-    'IMG450': 'https://user-images.githubusercontent.com/22366935/129747665-8be62ce6-ff9a-4569-889a-6cc79272ef78.png',
-    'IMG451': 'https://user-images.githubusercontent.com/22366935/129753259-d4b00d74-1287-4233-975e-29c19f822c1a.png',
-    'IMG452': 'https://user-images.githubusercontent.com/22366935/129787330-069921da-a3bf-4815-9cf1-0f30a6025234.png',
-}
 
 
 if 'is_login' not in ss:
@@ -116,7 +100,7 @@ with tab1:
         if len(selected_info2['rows']):
             ri = selected_info2['rows'][0]
             pt = df_selected.loc[ri, 'PieceType']
-            url = pt_image.get(pt)
+            url = PT_IMAGE.get(pt)
             st.markdown(f'**{pt}**')
             if url:
                 st.image(url, width=250)
