79e091faf6bcd95355f3db44ec557428982c0ca0
fsmosca <fsmosca@users.noreply.github.com>
2024-06-16 09:12:03 +0800

    adjust logo size

--- pages/piece_value.py
+++ pages/piece_value.py
@@ -13,11 +13,16 @@ st.set_page_config(layout='wide', page_title=st.secrets['page_title'])
 
 st.markdown("""
     <style>    
-           /* Remove blank space at top and bottom */ 
-           .block-container {
-               padding-top: 1.5rem;
-               padding-bottom: 1rem;
-            }
+        /* Remove blank space at top and bottom */ 
+        .block-container {
+            padding-top: 1.5rem;
+            padding-bottom: 1rem;
+        }
+            
+        /*Adjust logo size*/
+        [alt=Logo] {
+            height: 3rem;
+        }
     </style>
     """, unsafe_allow_html=True)
 
